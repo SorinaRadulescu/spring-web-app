e2432e4924fdfcc80dbb2ed049b8937f51ad36b7
SorinaRadulescu <radulescu.sorina123@yahoo.com>
2023-12-05 17:28:12 +0200

    added Publisher class and repo

--- src/main/java/com/example/learningspring/bootstrap/BootStrapData.java
+++ src/main/java/com/example/learningspring/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.example.learningspring.bootstrap;
 
 import com.example.learningspring.domain.Author;
 import com.example.learningspring.domain.Book;
+import com.example.learningspring.domain.Publisher;
 import com.example.learningspring.repositories.AuthorRepository;
 import com.example.learningspring.repositories.BookRepository;
+import com.example.learningspring.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,19 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
+        //Publisher mainPublisher = new Publisher("Publisher Name", "Address Line 1", "Bucharest", "Romania", "1234567");
 
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
@@ -39,5 +44,15 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
+
+        Publisher publisher = new Publisher();
+        publisher.setName("Publisher Name");
+        publisher.setCity("Publisher City");
+        publisher.setState("Publisher State");
+        publisher.setAddressLine1("Address Line 1");
+        publisher.setZip("1234567");
+
+        publisherRepository.save(publisher);
+        System.out.println("Publisher count: " + publisherRepository.count());
     }
 }
